2370344b1f896e5b39cce8dd81027ae186b4c40a
romain.rousserie <romain.rousserie@modisfrance.fr>
2017-09-20 17:02:46 +0200

    Fixing Chart Module

--- frontend/src/app/datas/data.module.ts
+++ frontend/src/app/datas/data.module.ts
@@ -2,18 +2,34 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DataRoutingModule } from './data-routing.module';
 import { ChartModule } from 'angular2-highcharts';
-import { MeasureComponent } from './measure/measure.component';
+import { HighchartsStatic } from 'angular2-highcharts/dist/HighchartsService';
 
+import { MeasureComponent } from './measure/measure.component';
 import { SharedModule } from '../shared/shared.module';
+
+declare var require: any;
+
+export function highchartsFactory() {
+  const hc = require('highcharts/highstock');
+  const dd = require('highcharts/modules/exporting');
+  dd(hc);
+  return hc;
+}
 
 @NgModule({
   imports: [
     SharedModule,
-    ChartModule.forRoot(require('highcharts')),
+    ChartModule,
     DataRoutingModule,
   ],
   declarations: [
     MeasureComponent
+  ],
+  providers: [
+    {
+      provide: HighchartsStatic,
+      useFactory: highchartsFactory
+    }
   ]
 })
 export class DataModule { }
